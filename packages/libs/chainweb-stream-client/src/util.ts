1ca400283e2e9140ae922029e97b3ecc6507c2dd
Dean Verjans <Developer.dean@outlook.com>
2026-05-17 19:43:57 +0200

    chore: convert chainweb-stream-client to oxlint

--- packages/libs/chainweb-stream-client/src/util.ts
+++ packages/libs/chainweb-stream-client/src/util.ts
@@ -3,10 +3,7 @@ export function isNotUndefined<T>(value: T | undefined): value is T {
 }
 
 export function isUndefinedOrNull<T>(
-  // eslint-disable-next-line @rushstack/no-new-null
   value: T | undefined | null,
-  // eslint-disable-next-line @rushstack/no-new-null
 ): value is null | undefined {
-  // eslint-disable-next-line @rushstack/no-new-null
   return value === null || value === undefined;
 }
