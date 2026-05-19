6c6dc97ca0d591a5b57ddd18facebb3731782b2e
Dean Verjans <developer.dean@outlook.com>
2026-05-19 15:35:48 +0200

    chore: convert chainweaver-wallet-connect-plugin to oxlint

--- packages/apps/chainweaver-wallet-connect-plugin/src/hooks/useWalletKit.ts
+++ packages/apps/chainweaver-wallet-connect-plugin/src/hooks/useWalletKit.ts
@@ -35,7 +35,6 @@ const useWalletKit = (sessionProposalHandler: (args: WalletKitTypes.SessionPropo
     if (!walletKitRef.current) {
       initializeWalletKit();
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return [walletKit, walletKitRef];
