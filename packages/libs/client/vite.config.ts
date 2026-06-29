a269337b1a72b34bd1d0522e23934f2fb84c6041
Dean Verjans <Developer.dean@outlook.com>
2026-06-29 22:05:00 +0200

    test(client): convert to vitest v4

--- packages/libs/client/vite.config.ts
+++ packages/libs/client/vite.config.ts
@@ -1,13 +1,36 @@
 import { defineLibConfig } from '@kda-community-dev/vite-config/lib';
-import { mergeConfig } from 'vite';
+import { defineBaseTestConfig } from '@kda-community-dev/vitest-config/base';
+import { mergeConfig, type UserConfig } from 'vite';
 
-export default mergeConfig(defineLibConfig(), {
-  build: {
-    lib: {
-      entry: {
-        index: './src/index.ts',
-        fp: './src/fp.ts',
+export default mergeConfig(
+  mergeConfig(defineLibConfig(), defineBaseTestConfig()),
+  {
+    build: {
+      lib: {
+        entry: {
+          index: './src/index.ts',
+          fp: './src/fp.ts',
+        },
       },
     },
-  },
-});
+    test: {
+      exclude: ['src/**/*.int.test.ts'],
+      coverage: {
+        exclude: [
+          // Type only files
+          'src/client/interfaces/interfaces.ts',
+          'src/interfaces/IPactCommand.ts',
+          'src/interfaces/ISigningRequest.ts',
+          'src/interfaces/type-utilities.ts',
+          'src/signing/eckoWallet/eckoTypes.ts',
+        ],
+        thresholds: {
+          lines: 99,
+          functions: 92,
+          branches: 96,
+          statements: 99,
+        },
+      },
+    },
+  } as UserConfig,
+);
