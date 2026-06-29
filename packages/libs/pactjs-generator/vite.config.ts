16c805763cdc5746e74963a4feec869794d2f6af
Dean Verjans <Developer.dean@outlook.com>
2026-06-29 22:05:01 +0200

    test(pactjs-generator): convert to vitest v4

--- packages/libs/pactjs-generator/vite.config.ts
+++ packages/libs/pactjs-generator/vite.config.ts
@@ -1,3 +1,5 @@
 import { defineNodeConfig } from '@kda-community-dev/vite-config/node';
+import { defineBaseTestConfig } from '@kda-community-dev/vitest-config/base';
+import { mergeConfig } from 'vite';
 
-export default defineNodeConfig();
+export default mergeConfig(defineNodeConfig(), defineBaseTestConfig());
