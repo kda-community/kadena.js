541003836b27d93c0608d5f93737494ada54b300
CryptoPascal31 <95504829+CryptoPascal31@users.noreply.github.com>
2026-07-03 13:42:22 +0000

    New test

--- packages/e2e/e2e-base/src/page-objects/tools-app/pages/faucet/fund-new-account.page.ts
+++ packages/e2e/e2e-base/src/page-objects/tools-app/pages/faucet/fund-new-account.page.ts
@@ -30,13 +30,14 @@ export class FundNewAccountPage {
   }
 
   public async CreateFundAccount(account: IAccount): Promise<void> {
+    console.log(account)
     for (const keyPair of account.keys) {
       console.log(keyPair.publicKey)
       await this._publicKey.fill(keyPair.publicKey);
       await this._addPubKey.click();
     }
     console.log("--------------------------------")
-    await this._chainId.setValue(account.chains[0]);
+    //await this._chainId.setValue(account.chains[0]);
     //Form validation is retriggered after setting the chain. Explicitly wait for the Account Name to be visible before pressing fund.
     await expect(this._accountNameInput).toHaveValue(account.account);
     await this._createAndFundBtn.click();
