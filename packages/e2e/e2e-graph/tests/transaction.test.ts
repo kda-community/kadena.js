6e64741088f4025dde75ea2c6d273736bfc85cc6
CryptoPascal31 <95504829+CryptoPascal31@users.noreply.github.com>
2026-07-06 22:13:45 +0000

    Test this one

--- packages/e2e/e2e-graph/tests/transaction.test.ts
+++ packages/e2e/e2e-graph/tests/transaction.test.ts
@@ -35,6 +35,7 @@ import {
 
 test.describe('Query: getTransactions', () => {
   test('Query: getTransactions - Same Chain Transfer', async ({ request }) => {
+    test.setTimeout(60_000);
     // declare testcase scoped variables.
     let query: any;
     let sourceAccount: IAccount;
@@ -240,7 +241,7 @@ test.describe('Query: getTransactions', () => {
         expect(finalResponse.transactions.edges[0].node).toEqual({
           result: {
             continuation: `{"step":1,"yield":null,"pactId":"${transfer.continuation?.pactId}","executed":null,"stepCount":2,"continuation":{"def":"coin.transfer-crosschain","args":["${sourceAccount.account}","${targetAccount.account}",{"keys":["${targetAccount.keys[0].publicKey}"],"pred":"keys-all"},"1",20]},"stepHasRollback":false}`,
-            gas: 210,
+            gas: 262,
             eventCount: 4,
             events: {
               edges: [
@@ -274,7 +275,7 @@ test.describe('Query: getTransactions', () => {
                 {
                   node: {
                     requestKey: transfer.reqKey,
-                    parameterText: `["${sourceAccount.account}","${devnetMiner.account}",2.1e-6]`,
+                    parameterText: `["${sourceAccount.account}","${devnetMiner.account}",2.62e-6]`,
                     id: base64Encode(
                       `Event:["${transfer.metaData?.blockHash}","3","${transfer.reqKey}"]`,
                     ),
@@ -322,7 +323,7 @@ test.describe('Query: getTransactions', () => {
                 },
                 {
                   node: {
-                    amount: 0.0000021,
+                    amount: 0.00000262,
                     transaction: {
                       result: {
                         block: {
